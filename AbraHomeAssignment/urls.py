6298c37d0485e2b6fdbe9fa9339098a4f77b8229
Ido Shamir <ishamir99@gmail.com>
2024-02-21 17:08:43 +0200

    update dependencies

--- AbraHomeAssignment/urls.py
+++ AbraHomeAssignment/urls.py
@@ -17,8 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from django.conf import settings
-from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     # add messages api urls
@@ -26,6 +25,3 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
-# add at the last
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
